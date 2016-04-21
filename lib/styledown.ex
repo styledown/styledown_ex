27a734c6432d1e74eb3905539b59c4224fed9c11
Victor Solis <hvictorsolis@gmail.com>
2016-04-22 05:59:47 +0800

    Adds documentation

--- lib/styledown.ex
+++ lib/styledown.ex
@@ -1,6 +1,18 @@
 defmodule Styledown do
+  @moduledoc """
+  Elixir integration of Styledown.
+  """
+
   @js_source Path.join(~w(#{File.cwd!} vendor styledown.js))
 
+  @doc ~S"""
+  Returns a string parsed by Styledown.
+
+  ## Example
+
+      iex> Styledown.parse("### hi")
+      "<section class=\"sg-block sg-section-hi\">\n  <h3 id=\"hi\" class=\"sg\">hi</h3>\n</section>"
+  """
   def parse(code, opts \\ [])
 
   def parse([h|_t] = code, opts) when is_binary(h) do
@@ -16,6 +28,14 @@ defmodule Styledown do
     Execjs.call context, "Styledown.parse", [code, opts]
   end
 
+  @doc """
+  Returns the styledown.js version.
+
+  ## Example
+
+      iex> Styledown.js_version
+      "1.0.1"
+  """
   def js_version, do: Execjs.eval source <> "Styledown.version"
 
   defp source, do: File.read! @js_source
